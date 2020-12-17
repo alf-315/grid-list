f086dcd2facbc17dd9259495a887c88f31d759eb
Brian Curtain <dc.brian.07@gmail.com>
2020-12-17 18:43:40 +0800

    third commit

--- dist/grid-list/main.js
+++ dist/grid-list/main.js
@@ -83,19 +83,88 @@ function AppComponent_div_1_Template(rf, ctx) { if (rf & 1) {
 class AppComponent {
     constructor() {
         this.title = 'grid-list';
-        this.products = [];
+        this.products = [
+            {
+                "productId": "1",
+                "imagePath": "https://picsum.photos/300",
+                "rating": "4.9",
+                "percentage": "99",
+                "tags": "1.2K",
+                "title": "Xiamoi Wifi Extender",
+                "price": 12.0,
+                "ePacket": true,
+                "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+            },
+            {
+                "productId": "2",
+                "imagePath": "https://picsum.photos/300",
+                "rating": "4.9",
+                "percentage": "99",
+                "tags": "1.2K",
+                "title": "Xiamoi Wifi Extender",
+                "price": 12.0,
+                "ePacket": true,
+                "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+            },
+            {
+                "productId": "3",
+                "imagePath": "https://picsum.photos/300",
+                "rating": "4.9",
+                "percentage": "99",
+                "tags": "1.2K",
+                "title": "Xiamoi Wifi Extender",
+                "price": 12.0,
+                "ePacket": true,
+                "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+            },
+            {
+                "productId": "4",
+                "imagePath": "https://picsum.photos/300",
+                "rating": "4.9",
+                "percentage": "99",
+                "tags": "1.2K",
+                "title": "Xiamoi Wifi Extender",
+                "price": 12.0,
+                "ePacket": true,
+                "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+            },
+            {
+                "productId": "5",
+                "imagePath": "https://picsum.photos/300",
+                "rating": "4.9",
+                "percentage": "99",
+                "tags": "1.2K",
+                "title": "Xiamoi Wifi Extender",
+                "price": 12.0,
+                "ePacket": true,
+                "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+            },
+            {
+                "productId": "6",
+                "imagePath": "https://picsum.photos/300",
+                "rating": "4.9",
+                "percentage": "99",
+                "tags": "1.2K",
+                "title": "Xiamoi Wifi Extender",
+                "price": 12.0,
+                "ePacket": true,
+                "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+            },
+            {
+                "productId": "7",
+                "imagePath": "https://picsum.photos/300",
+                "rating": "4.9",
+                "percentage": "99",
+                "tags": "1.2K",
+                "title": "Xiamoi Wifi Extender",
+                "price": 12.0,
+                "ePacket": true,
+                "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+            }
+        ];
     }
     ngOnInit() {
-        this.getProduts();
-    }
-    getProduts() {
-        fetch('http://localhost:4200/api/Products').then(res => res.json().then(response => {
-            this.products = response;
-        }).catch(error => {
-            console.log(error);
-        })).catch(error => {
-            console.log(error);
-        });
+        // this.getProduts();
     }
 }
 AppComponent.ɵfac = function AppComponent_Factory(t) { return new (t || AppComponent)(); };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,97 @@ import { OnInit } from '@angular/core'
 })
 export class AppComponent implements OnInit {
   title = 'grid-list';
-  products = [];
+  products = [
+    {
+      "productId": "1",
+      "imagePath": "https://picsum.photos/300",
+      "rating": "4.9",
+      "percentage": "99",
+      "tags": "1.2K",
+      "title": "Xiamoi Wifi Extender",
+      "price": 12.0,
+      "ePacket": true,
+      "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+    },
+    {
+      "productId": "2",
+      "imagePath": "https://picsum.photos/300",
+      "rating": "4.9",
+      "percentage": "99",
+      "tags": "1.2K",
+      "title": "Xiamoi Wifi Extender",
+      "price": 12.0,
+      "ePacket": true,
+      "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+    },
+    {
+      "productId": "3",
+      "imagePath": "https://picsum.photos/300",
+      "rating": "4.9",
+      "percentage": "99",
+      "tags": "1.2K",
+      "title": "Xiamoi Wifi Extender",
+      "price": 12.0,
+      "ePacket": true,
+      "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+    },
+    {
+      "productId": "4",
+      "imagePath": "https://picsum.photos/300",
+      "rating": "4.9",
+      "percentage": "99",
+      "tags": "1.2K",
+      "title": "Xiamoi Wifi Extender",
+      "price": 12.0,
+      "ePacket": true,
+      "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+    },
+    {
+      "productId": "5",
+      "imagePath": "https://picsum.photos/300",
+      "rating": "4.9",
+      "percentage": "99",
+      "tags": "1.2K",
+      "title": "Xiamoi Wifi Extender",
+      "price": 12.0,
+      "ePacket": true,
+      "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+    },
+    {
+      "productId": "6",
+      "imagePath": "https://picsum.photos/300",
+      "rating": "4.9",
+      "percentage": "99",
+      "tags": "1.2K",
+      "title": "Xiamoi Wifi Extender",
+      "price": 12.0,
+      "ePacket": true,
+      "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+    },
+    {
+      "productId": "7",
+      "imagePath": "https://picsum.photos/300",
+      "rating": "4.9",
+      "percentage": "99",
+      "tags": "1.2K",
+      "title": "Xiamoi Wifi Extender",
+      "price": 12.0,
+      "ePacket": true,
+      "createdAt": "October 6, 2020 at 9:17:15 AM UTC-4"
+    }
+  ];
 
   ngOnInit() {
-    this.getProduts();
+    // this.getProduts();
   }
 
-  getProduts() {
-    fetch('http://localhost:4200/api/Products').then(res => res.json().then(response => {
-      this.products = response;
-    }).catch(error => {
-      console.log(error)
-    })).catch(error => {
-      console.log(error);
-    });
-  }
+  // getProduts() {
+  //   fetch('http://localhost:4200/api/Products').then(res => res.json().then(response => {
+  //     this.products = response;
+  //   }).catch(error => {
+  //     console.log(error)
+  //   })).catch(error => {
+  //     console.log(error);
+  //   });
+  // }
 }
